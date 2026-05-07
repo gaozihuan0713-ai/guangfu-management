0314a6a5e5ce4b18423fa03ad10c53e020f30f64
gaozihuan0713-ai <gaozihuan0713@gmail.com>
2026-05-07 20:12:52 +0800

    web

--- backend/api/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-API蓝图包
-"""
-from backend.api.auth import auth_bp
-from backend.api.data import data_bp
-from backend.api.alarm import alarm_bp
-
-__all__ = ['auth_bp', 'data_bp', 'alarm_bp']
--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-数据库模型包
-"""
-from backend.models.models import db, User, AlarmRecord, OperationLog
-
-__all__ = ['db', 'User', 'AlarmRecord', 'OperationLog']
